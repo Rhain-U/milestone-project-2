c6ed9f0704e402f57b01e57fa8d8cc3e9d012d54
Rhain-U-Code-institute <rhainu@hotmail.co.uk>
2025-03-13 18:09:57 +0000

    remove all code to start again due to unknown bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,15 @@
-/**Global variables */
-const squares = document.querySelectorAll('.square');
-const mole = document.querySelector('.mole');
+/**General styles */
+const  square = document.querySelectorAll('.square');
+const goodMole = document.querySelectorAll('.good-mole');
+const badMole = document.querySelectorAll('.bad-mole');
 const timeLeft = document.querySelector('#time-left');
-const score = document.querySelector('#score');
+let score = document.querySelector('#score');
+const startGame = document.querySelector('#start-game');
+const endGame = document.querySelector('#end-game');
 
+/**Game variables */
 let result = 0;
-let currentTime = 60
-let hitPosition; 
+let hitPosition;
+let currentTime = 60;   //60 seconds
 let timerId = null;
 
-/**function to add good mole to random square */
-
-function addBadMole() {
-    squares.forEach(square => {
-        square.classList.remove('bad-avo');
-    })
-
-    let randomSquare = squares[Math.floor(Math.random() * 9)];
-    randomSquare.classList.add('bad-avo');
-    
-hitPosition = randomSquare.id;
-}
-
-/**Add function for Good mole to pop up in random square */
-
-function addGoodMole() {
-    squares.forEach(square => {
-        square.classList.remove('good-avo');
-    })
-
-    let randomSquare = squares[Math.floor(Math.random() * 9)];
-    randomSquare.classList.add('good-avo');
-}
-
-addGoodMole();
-
-/**function to make score increase once the correct avo has been hit. */
-
-squares.forEach(square => {
-    square.addEventListener('mousedown', () => {
-        if (square.id === hitPosition) {
-            result++;
-            score.textContent = result+1;
-            hitPosition = null;
-        }
-    });
-});
-
-/**function to move the Avo's to a different square every few seconds */ 
-
-function moveBadAvo() {
-  timerId = setInterval(addBadMole, 1000); // 3000 milliseconds, 3 seconds
-}
-moveBadAvo();
-
-function moveGoodAvo() {
-    timerId = setInterval(addGoodMole, 500); // 3000 milliseconds, 3 seconds
-  }
-
-  //* Function to count down the time left */
-
-  timerId = setInterval(countDown, 1000);
-  
-function countDown() {
-    currentTime--;
-    timeLeft.textContent = currentTime;
-
-    if (currentTime === 0) {
-        clearInterval(timerId);
-        alert('Game Over! Your final score is ' + result);
-    } 
-}
