667dc9d5a3407f700e9267097c00a1f915f6f908
Rhain-U-Code-institute <rhainu@hotmail.co.uk>
2025-03-13 13:01:25 +0000

    Add automatic hole movent and scoreboard increase when clicked on correct hole.

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,22 @@ function randomSquare() {
 
 randomSquare();
 
+/**function to make score increase once the correct avo has been hit. */
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id === hitPosition) {
+            result++;
+            score.textContent = result+1;
+            hitPosition = null;
+        }
+    })
+})
+
+/**function to move the Avo's to a different square every second */ 
+
+function moveGoodAvo() {
+    timerId = setInterval(randomSquare, 2000);
+}
+
+moveGoodAvo(); 
