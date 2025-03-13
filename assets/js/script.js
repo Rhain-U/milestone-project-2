2034e428d819dd62190e83ee603c19c7823130f3
Rhain-U-Code-institute <rhainu@hotmail.co.uk>
2025-03-13 14:01:56 +0000

    Add both good and bad mole to appear in random squares.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,21 +9,31 @@ let currentTime = 60
 let hitPosition; 
 let timerId = null;
 
-/**function to add mole to random square */
+/**function to add good mole to random square */
 
-function randomSquare() {
+function addBadMole() {
+    squares.forEach(square => {
+        square.classList.remove('bad-avo');
+    })
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)];
+    randomSquare.classList.add('bad-avo');
+    
+hitPosition = randomSquare.id;
+}
+
+/**Add function for Good mole to pop up in random square */
+
+function addGoodMole() {
     squares.forEach(square => {
         square.classList.remove('good-avo');
     })
 
     let randomSquare = squares[Math.floor(Math.random() * 9)];
     randomSquare.classList.add('good-avo');
-
-    hitPosition = randomSquare.id;
-
 }
 
-randomSquare();
+addGoodMole();
 
 /**function to make score increase once the correct avo has been hit. */
 
@@ -34,13 +44,17 @@ squares.forEach(square => {
             score.textContent = result+1;
             hitPosition = null;
         }
-    })
-})
+    });
+});
 
 /**function to move the Avo's to a different square every second */ 
 
-function moveGoodAvo() {
-    timerId = setInterval(randomSquare, 2000);
+function moveBadAvo() {
+  timerId = setInterval(addBadMole, 1000); // 3000 milliseconds, 3 seconds
 }
+moveBadAvo();
 
-moveGoodAvo(); 
+function moveGoodAvo() {
+    timerId = setInterval(addGoodMole, 1000); // 3000 milliseconds, 3 seconds
+  }
+
